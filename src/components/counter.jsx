470d9ff96c8be66882516868b597ae4c01cb4fcc
Gabriel Chen <gabriel.jchen@gmail.com>
2021-08-05 14:21:03 -0400

    change class dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,23 @@ import React, {useState} from 'react'
 
 export default function Counter() {
   const [state, setState] = useState({
-    count: 1,
+    count: 0,
     imgUrl: 'https://picsum.photos/200'
   }) 
   function Change(){
     return state.count === 0 ? 'Zero' : state.count
   }
+
+  function getClass(){
+    let classes = 'badge m-2 badge-';
+    classes += state.count === 0 ? 'warning' : 'primary';
+    return classes
+  }
   return(
     <React.Fragment>
       <img src={state.imgUrl} alt=""/>
-      <span className='badge badge-primary'>{ Change() }</span>
-      <button>Increment</button>
+      <span className={getClass()}>{ Change() }</span>
+      <button className='btn btn-primary'>Increment</button>
     </React.Fragment>
   )
 }
