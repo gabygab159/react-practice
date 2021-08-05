48ade83b7fd42208b626a05ad19bdc099b654df1
Gabriel Chen <gabriel.jchen@gmail.com>
2021-08-05 13:51:59 -0400

    create counter component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css'
+import Counter from "./components/counter";
 
 const element = <h1>hello world</h1>
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(<Counter/>, document.getElementById('root'))
