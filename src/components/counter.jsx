b8faf36abd33c4f1911f9d6b0c1ce4a57a7bbf2c
Gabriel Chen <gabriel.jchen@gmail.com>
2021-08-06 13:48:50 -0400

    practice setting state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,38 +1,47 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function Counter() {
   const [state, setState] = useState({
-    count: 0,
-    imgUrl: 'https://picsum.photos/200',
-    tags:['tag1', 'tag2']
-  }) 
+    imgUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2"],
+  });
+
+  const [count, setCount] = useState(0);
 
   function renderTags() {
-    if(state.tags.length === 0) return <p>'there are no tags'</p>;
-    return  <ul>
-              <li>
-                {state.tags.map(tag =><li key={tag}>{tag}</li>)}
-              </li>
-            </ul>
+    if (state.tags.length === 0) return <p>'there are no tags'</p>;
+    return (
+      <ul>
+        <li>
+          {state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </li>
+      </ul>
+    );
   }
-  function Change(){
-    return state.count === 0 ? 'Zero' : state.count
+  function Change() {
+    return count === 0 ? "Zero" : count;
   }
 
-  function getClass(){
-    let classes = 'badge m-2 badge-';
-    classes += state.count === 0 ? 'warning' : 'primary';
-    return classes
+  function getClass() {
+    let classes = "badge m-2 badge-";
+    classes += count === 0 ? "warning" : "primary";
+    return classes;
   }
-  return(
+
+  function handleIncrement() {
+    setCount(count + 1);
+    console.log(count);
+  }
+  return (
     <React.Fragment>
-      <img src={state.imgUrl} alt=""/>
-      <span className={getClass()}>{ Change() }</span>
-      <button className='btn btn-primary'>Increment</button>
+      <img src={state.imgUrl} alt="" />
+      <span className={getClass()}>{Change()}</span>
+      <button onClick={handleIncrement} className="btn btn-primary">
+        Increment
+      </button>
       {renderTags()}
-      {/* <ul><li>
-      {state.tags.map(tag =><li key={tag}>{tag}</li>)}
-      </li></ul> */}
     </React.Fragment>
-  )
+  );
 }
